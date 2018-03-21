60cd4bfae7c87da4f17f8e98b9f6597975b960ba
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-21 16:32:21 -0100

    deleted users route and app.js user info

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
 
@@ -15,7 +14,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
