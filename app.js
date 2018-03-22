2bcd33ecd790a158b789c9ac64a427867d20fdb8
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-22 03:04:54 -0100

    added user model

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:4200']
+}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
